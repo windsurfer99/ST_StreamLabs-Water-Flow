0f6bfd8424d824da00ec17ba7009468ea71e777f
Bruce Andrews <bruceandrews@ieee.org>
2019-04-21 16:18:05 -0400

    Updated with the debug ability to update homeAway status in water flow meter cloud

--- smartapps/windsurfer99/streamlabs-water-flow-sm.src/streamlabs-water-flow-sm.groovy
+++ smartapps/windsurfer99/streamlabs-water-flow-sm.src/streamlabs-water-flow-sm.groovy
@@ -26,18 +26,17 @@ definition(
     appSetting "api_key"
 }
 
-
 preferences {
 	page(name: "pageOne", title: "Options", uninstall: true, install: true) {
 		section("Inputs") {
         		paragraph ("Set the API Key via App Settings in IDE")
             		label (title: "Assign a name for Service Manager", required: false, multiple: true)
             		input (name: "modes", type: "mode", title: "Enter SmartThings modes when water meter should be Away", multiple: true, required: false)
-            		input (name: "locName", type: "text", title: "Enter location name assigned to Streamlabs meter", multiple: false, required: true)
+            		input (name: "SL_locName", type: "text", title: "Enter Streamlabs location name assigned to Streamlabs flow meter", multiple: false, required: true)
 		}
 	}
 }
-	
+
 def installed() {
 	log.debug "Installed with settings: ${settings}"
 	// get the value of api key
@@ -46,7 +45,7 @@ def installed() {
     //log.debug "Modes: $modes"
 
 	initialize()
-    GetLocations()
+    initSL_Locations()
 }
 
 def updated() {
@@ -61,47 +60,83 @@ def initialize() {
 
 }
 
-def GetLocations() {
+//Get desired location from Streamlabs cloud based on user's entered location's name
+def initSL_Locations() {
     def params = [
-        uri:  'https://api.streamlabswater.com/v1/locations',
-        headers: ['Authorization': 'Bearer ' + appSettings.api_key],
-        contentType: 'application/json',
+            uri:  'https://api.streamlabswater.com/v1/locations',
+            headers: ['Authorization': 'Bearer ' + appSettings.api_key],
+            contentType: 'application/json',
     ]
-    log.debug "params for locations: ${params}"
-//    def TotalLocations
-try {
-		def location
+    //log.debug "params for locations: ${params}"
+
+	try {
+		def SL_location
         httpGet(params) {resp ->
             //log.debug "resp: ${resp}"
             def resp_data = resp.data
             //log.debug "resp data: ${resp.data}"
-            log.debug "resp_data: ${resp_data}"
-            def locations0 = resp_data.locations[0]
-            log.debug "locations0: ${locations0}"
+            //log.debug "resp_data: ${resp_data}"
+            def SL_locations0 = resp_data.locations[0]
+            //log.debug "SL_locations0: ${SL_locations0}"
             def ttl = resp_data.total
-            log.debug "Total locations: ${ttl}"
-            resp.data.locations.each{ loc->
-            	log.debug "name of location: ${loc.name}"
-                if (loc.name == locName) {
-                	state.location = loc
+            log.debug "Total SL_locations: ${ttl}"
+            resp.data.locations.each{ SL_loc->
+            	//log.debug "name of SL_location: ${SL_loc.name}"
+                if (SL_loc.name == SL_locName) {
+                	state.SL_location = SL_loc
                 }
             }
-            location = state.location
-            log.debug "location to use: ${location}"
-            //log.debug "# of locations: ${resp.data.total}"
+            SL_location = state.SL_location
+            log.debug "SL_location to use: ${SL_location}"
+            //log.debug "# of SL_locations: ${resp.data.total}"
             //log.debug "resp contentType: ${resp.contentType}"
             //def totl = resp.data.locations[0].name
             //log.debug "name of locations[0]: ${totl}"
  
-            log.debug "resp status: ${resp.status}"
-            if (resp.status == 200){
-                log.debug "resp status good"
-            }
+updateAway()
+            //log.debug "initSL_Locations resp status: ${resp.status}"
+            //if (resp.status == 200){
+            //    log.debug "initSL_Locations resp status good"
+            //}
 
         }
     } catch (e) {
-        log.error "error in locations: $e"
+        log.error "error in SL_locations: $e"
     }
 }
+
+//Set Away status
+def updateAway() {
+	def newHomeAway
+    //log.debug "state.SL_location.homeAway: ${state.SL_location.homeAway}"
+    if (state.SL_location.homeAway == "home") {
+    	newHomeAway = "away"
+    } else {
+    	newHomeAway = "home"
+    }
+    //log.debug "newHomeAway: ${newHomeAway}"
+    def cmdBody = [
+			"homeAway": newHomeAway
+	]
+    //log.debug "cmdBody: ${cmdBody}"
+    def params = [
+            uri:  'https://api.streamlabswater.com/v1/locations/' + state.SL_location.locationId,
+            headers: ['Authorization': 'Bearer ' + appSettings.api_key],
+            contentType: 'application/json',
+			body : new groovy.json.JsonBuilder(cmdBody).toString()    
+            ]
+
+    log.debug "params for updateAway: ${params}"
+
+	try {
+        httpPutJson(params){resp ->
+            log.debug "updateAway resp data: ${resp.data}"
+            log.debug "updateAway resp status: ${resp.status}"
+        }
+    } catch (e) {
+        log.error "error in updateAway: $e"
+    }
+}
+
 
 // TODO: implement event handlers
