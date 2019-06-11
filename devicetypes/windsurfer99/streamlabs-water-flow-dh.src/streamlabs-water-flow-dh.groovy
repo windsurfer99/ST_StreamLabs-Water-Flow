b5a88b9e1b54f0002ef80dc12d8ade4c113e96f5
Bruce Andrews <bruceandrews@ieee.org>
2019-06-11 00:39:06 -0400

    Version 1.0 working; using logger for debug output

--- devicetypes/windsurfer99/streamlabs-water-flow-dh.src/streamlabs-water-flow-dh.groovy
+++ devicetypes/windsurfer99/streamlabs-water-flow-dh.src/streamlabs-water-flow-dh.groovy
@@ -1,6 +1,7 @@
 /**
  *  StreamLabs Water Flow DH
- *  Device Handler for StreamLabs Water Flow Meter: Cloud Connected Device
+ *  Device Handler for StreamLabs Water Flow Meter: Cloud Connected Device; created by StreamLabs Water Flow Service Manager/ SmartApp
+ *  Version 1.0
  *
  *  Copyright 2019 windsurfer99
  *
@@ -44,8 +45,6 @@ metadata {
 			state "default", label:"Refresh", icon:"st.secondary.refresh", action:"refresh.refresh"
 		}
 		standardTile("suspend", "device.suspend", decoration: "flat", width: 2, height: 2) {
-//			state "pause", label: "pause", icon:"st.sonos.play-btn", action:"changeToMonitor", backgroundColor:"#e86d13"
-//			state "monitor", label: "monitoring", defaultState:true, icon:"st.sonos.pause-btn", action:"changeToPause", backgroundColor:"#00A0DC"
 			state "pause", label: "Now paused", icon:"st.sonos.play-btn", action:"changeToMonitor"
 			state "monitor", label: "Now monitoring", defaultState:true, icon:"st.sonos.pause-btn", action:"changeToPause"
 		}		
@@ -60,10 +59,6 @@ metadata {
 			state "todayFlow", label: 'Usage Today ${currentValue} Gal.'
 		}
 
-//		valueTile("todayFlow", "device.todayFlow", decoration: "flat", height: 2, width: 2) {
-//			state "todayFlow", label: 'Usage Today\n\r ${currentValue} Gal.'
-//		}
-
 		valueTile("monthFlow", "device.monthFlow", decoration: "flat", height: 2, width: 2) {
 			state "monthFlow", label: 'Usage this month ${currentValue} Gal.'
 		}
@@ -76,55 +71,72 @@ metadata {
 		details(["water", "homeAway", "suspend", "refresh", "todayFlow", "monthFlow", "yearFlow"])
     }
     preferences {
-        input name: "pauseDelay", type: "number", title: "Number", description: "# of minutes for max. pause:", required: false
+        input (name: "pauseDelay", type: "number", title: "Number", description: "# of minutes for max. pause:", required: false)
+        input (name: "zwtLoggingLevelIDE",
+            title: "IDE Live Logging Level:\nMessages with this level and higher will be logged to the IDE.",
+            type: "enum",
+            options: [
+                "0" : "None",
+                "1" : "Error",
+                "2" : "Warn",
+                "3" : "Info",
+                "4" : "Debug",
+                "5" : "Trace"
+            ],
+            defaultValue: "3",
+            required: false
+        )
 	}
 }
 //required implementations
 def installed() {
-	log.debug "StreamLabs DH installed; "
+	log.debug "StreamLabs DH installed() called"
 
 	runIn(7,"initialize")
-	//initialize()
-//    refresh()
 }
 
 def initialize() {
-	log.debug "StreamLabs DH initialize with pause timeout: ${pauseDelay}"
+    // Logging Level:
+    state.loggingLevelIDE = (settings.zwtLoggingLevelIDE) ? settings.zwtLoggingLevelIDE.toInteger() : 3
+
+	log.debug "StreamLabs DH initialize() called with pause timeout: ${pauseDelay}"
     schedule("0 0/10 * * * ?", poll) //refresh every 10 minutes
 	generateEvent([name:"suspend", value:"monitor"])
     state.wetDry = "dry" //real status even if paused
-//    device.suspend = "monitor"
-//    refresh()
 }
 
+/**
+ *  updated()
+ *
+ *  Runs when the user hits "Done" from Settings page.
+ *
+ *  Note: Weirdly, update() seems to be called twice. tried solutions described here but didn't work
+ *  (I'm guessing it is a re-entrance issue). See: https://community.smartthings.com/t/updated-being-called-twice/62912
+ **/
 def updated(){
-	log.debug "StreamLabs DH updated"
-	unschedule("poll")
-	runIn(7,"initialize")
+    // Logging Level:
+    state.loggingLevelIDE = (settings.zwtLoggingLevelIDE) ? settings.zwtLoggingLevelIDE.toInteger() : 3
+
+    logger("StreamLabs DH updated() called","trace")
+    unschedule("poll")
+    runIn(7,"initialize")
 }
-/*
-def uninstalled() {
-	log.debug "StreamLabs DH uninstalled called for ${device.deviceNetworkId}"
-    //delete me from parent Service Manager; this will leave in a strange state until parent recreates me
-	parent.deleteSmartLabsDevice(device.deviceNetworkId)
-}
-*/
 
 // parse events into attributes; not really used with this type of Device Handler
 def parse(String description) {
-	log.debug "StreamLabs DH- parse called with '${description}'"
+    logger("StreamLabs DH iparse called with '${description}","trace")
 
 }
 //poll for changes and update locally; framework is supposed to call every 10 minutes but doesn't seem to
 //so scheduled internally, this is the handler for that
 def poll() {
-	log.debug "StreamLabs DH poll called"
+    logger("StreamLabs DH poll() called","trace")
     refresh()
 }
 
 def refresh(){
 	def cloudData = parent.retrievecloudData() 
-	log.debug "StreamLabs DH refresh- cloudData: ${cloudData}, suspend: ${device.currentValue("suspend")}"
+    logger("StreamLabs DH refresh() called- cloudData: ${cloudData}, suspend: ${device.currentValue("suspend")}","debug")
     state.todayFlow = cloudData.todayFlow
     state.thisMonthFlow = cloudData.thisMonthFlow
     state.thisYearFlow = cloudData.thisYearFlow
@@ -144,36 +156,67 @@ def refresh(){
 
 //Tile action to change StreamLabs to home
 def changeToHome() {
-	log.debug "StreamLabs DH changeToHome called"
+    logger("StreamLabs DH changeToHome() called","debug")
 	parent.updateAway("home")
 }
 
 //Tile action to change StreamLabs to away
 def changeToAway() {
-	log.debug "StreamLabs DH changeToAway called"
+    logger("StreamLabs DH changeToAway() called","debug")
 	parent.updateAway("away")
 }
 
 //Tile action (& suspend time limit action) to re-enble monitoring StreamLabs for alerts
 def changeToMonitor() {
-	log.debug "StreamLabs DH changeToMonitor called"
+    logger("StreamLabs DH changeToMonitor() called","debug")
 	generateEvent([name:"suspend", value:"monitor"])
 	generateEvent([name:"water", value:state.wetDry]) //update real status in case it had been suspended
 }
 
 //Tile action to pause monitoring StreamLabs for alerts
 def changeToPause() {
-	log.debug "StreamLabs DH changeToPause called"
+    logger("StreamLabs DH changeToPause() called","debug")
 	generateEvent([name:"suspend", value:"pause"])
 	generateEvent([name:"water", value:"dry"]) //remove wet while paused
     if (pauseDelay > 0) {//if user wants a time limit on suspending alerts
 	    runIn (pauseDelay*60, "changeToMonitor")
     }
 }
+/**
+ *  logger()
+ *
+ *  Wrapper function for all logging. Thanks codersaur.
+ **/
+private logger(msg, level = "debug") {
+    switch(level) {
+        case "error":
+            if (state.loggingLevelIDE >= 1) log.error msg
+            break
 
+        case "warn":
+            if (state.loggingLevelIDE >= 2) log.warn msg
+            break
+
+        case "info":
+            if (state.loggingLevelIDE >= 3) log.info msg
+            break
+
+        case "debug":
+            if (state.loggingLevelIDE >= 4) log.debug msg
+            break
+
+        case "trace":
+            if (state.loggingLevelIDE >= 5) log.trace msg
+            break
+
+        default:
+            log.debug msg
+            break
+    }
+}
 //handle Events sent from Service Manager; typically wet & dry
 def generateEvent(Map results) {
-	log.debug "StreamLabs DH generateEvent called with parameters: '${results}'"
+    logger("StreamLabs DH generateEvent() called with parameters: '${results}'","debug")
 	sendEvent(results)
 	return null
 }
@@ -181,7 +224,7 @@ def generateEvent(Map results) {
 //Typically called by parent: update to "wet"
 def changeWaterToWet() {
 	def suspension = device.currentValue("suspend")
-	log.debug "StreamLabs DH changeWaterToWet called with suspend: ${suspension}"
+    logger("StreamLabs DH changeWaterToWet() called with suspend: ${suspension}","info")
     state.wetDry = "wet"
     if (suspension != "pause") { //update tile displayed info only if not paused
 		generateEvent([name:"water", value:"wet"])
@@ -190,7 +233,7 @@ def changeWaterToWet() {
 
 //Typically called by parent: update to "dry"
 def changeWaterToDry() {
-	log.debug "StreamLabs DH changeWaterToDry called"
+    logger("StreamLabs DH changeWaterToDry() called","info")
     state.wetDry = "dry"
     //update even if paused
 	generateEvent([name:"water", value:"dry"])
